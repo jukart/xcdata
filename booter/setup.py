7ad4b2c77369d1898f0784b641240e48834110e0
Jürgen Kartnaller <juergen@kartnaller.at>
2017-04-22 12:12:32 +0200

    example settings

--- booter/setup.py
+++ booter/setup.py
@@ -1,12 +1,25 @@
 settings = {
     "loih": {
-        "profile": "loih.prf",
-        "airspace": ["lovv.desktop.strepla.openair.txt"],
-        "waipoint": ["Austria.cup", "ofm.cup"],
-        "map": "ALPS_HighRes.xcm",
-        "checklist": "loih.txt",
-        "events": "events.xci",
-        "plane": "D-5461-18m.xcp",
+        "links": {
+            "default.prf": "profiles/loih.prf",
+            "Default.tsk": None,
+            "airspace1.txt": "airspaces/lovv.desktop.strepla.openair.txt",
+            "airspace2.txt": None,
+            "waypoint1.cup": "waypoints/Austria.cup",
+            "waypoint2.cup": "waypoints/ofm.cup",
+            "map.txt": "maps/ALPS_HighRes.xcm",
+            "checklist.txt": "checklists/loih.txt",
+            "events.xci": "xcsoar/events.xci",
+            "plane.xcp": "planes/D-5461-18m.xcp",
+            "tasks": "tasks",
+        },
+        "templates": {
+            "templates": {
+                "default.prf": "profiles/loih.prf.jinja2",
+            },
+            "context": {
+            }
+        },
     },
     "sisteron": {
         "profile": "sisteron.prf",
