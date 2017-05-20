116719caa0c8840aee5da3759a2cd55082ce09ce
Jürgen Kartnaller <juergen@kartnaller.at>
2017-05-20 08:19:19 +0200

    aac2017

--- booter/setup.py
+++ booter/setup.py
@@ -18,12 +18,12 @@ settings = {
             "Default.tsk": None,
         },
     },
-    "aac2017": {
+    "aac-2017-speed": {
         "links": {
-            "data/airspace1.txt": "airspaces/lovv.desktop.strepla.openair.txt",
+            "data/airspace1.txt": "airspaces/AAC2016-2_openair.txt",
             "data/airspace2.txt": None,
             "data/airfields.txt": None,
-            "data/waypoint1.cup": "waypoints/Austria.cup",
+            "data/waypoint1.cup": "waypoints/AAC2016.cup",
             "data/waypoint2.cup": "waypoints/ofm.cup",
             "data/watched.cup": None,
             "data/map.xcm": "maps/ALPS_HighRes.xcm",
@@ -34,6 +34,25 @@ settings = {
         },
         "copy": {
             "default.prf": "profiles/competition.prf",
+            "Default.tsk": None,
+        },
+    },
+    "aac-2017-aat": {
+        "links": {
+            "data/airspace1.txt": "airspaces/AAC2016-2_openair.txt",
+            "data/airspace2.txt": None,
+            "data/airfields.txt": None,
+            "data/waypoint1.cup": "waypoints/AAC2016.cup",
+            "data/waypoint2.cup": "waypoints/ofm.cup",
+            "data/watched.cup": None,
+            "data/map.xcm": "maps/ALPS_HighRes.xcm",
+            "data/checklist.txt": "checklists/loih.txt",
+            "data/events.xci": "xcsoar/events.xci",
+            "data/plane.xcp": "planes/D-5461-18m.xcp",
+            "data/tasks": "tasks",
+        },
+        "copy": {
+            "default.prf": "profiles/aat.prf",
             "Default.tsk": None,
         },
     },
