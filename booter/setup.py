e609a133346feb095400fca4aa9cdae5f0d40e94
Jürgen Kartnaller <juergen@kartnaller.at>
2017-05-23 09:56:00 +0200

    speed profile

--- booter/setup.py
+++ booter/setup.py
@@ -33,6 +33,25 @@ settings = {
             "data/tasks": "tasks",
         },
         "copy": {
+            "default.prf": "profiles/speed.prf",
+            "Default.tsk": "tasks/aac-2017-offen-day2.tsk",
+        },
+    },
+    "aac-2017-aat": {
+        "links": {
+            "data/airspace1.txt": "airspaces/AAC2016-2_openair.txt",
+            "data/airspace2.txt": None,
+            "data/airfields.txt": None,
+            "data/waypoint1.cup": "waypoints/AAC2016.cup",
+            "data/waypoint2.cup": "waypoints/ofm.cup",
+            "data/watched.cup": None,
+            "data/map.xcm": "maps/ALPS_HighRes.xcm",
+            "data/checklist.txt": "checklists/loih.txt",
+            "data/events.xci": "xcsoar/events_aac.xci",
+            "data/plane.xcp": "planes/D-5461-18m.xcp",
+            "data/tasks": "tasks",
+        },
+        "copy": {
             "default.prf": "profiles/aat.prf",
             "Default.tsk": "tasks/aac-2017-offen-day2.tsk",
         },
